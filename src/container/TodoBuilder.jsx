417a2c8d4fe5756f57ae94a5fb246d6deccf5742
EmmanuelKumah <mailkumah@gmail.com>
2021-01-22 11:07:39 +0000

    Edit TodoBuilder.jsx

--- src/container/TodoBuilder.jsx
+++ src/container/TodoBuilder.jsx
@@ -1,29 +1,31 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
-import Header from "../components/UI/Header"
-import Hero from "../components/UI/Hero"
-import Features from "../components/UI/Features"
+import Header from "../components/UI/Header";
+import Hero from "../components/UI/Hero";
+import Features from "../components/UI/Features";
 
 function TodoBuilder() {
-    return (
-        <Router>
-            <Grid container direction="column">
-             <Grid item container > <Header /></Grid>
-             <Grid item container xs={12}>
-                 <Hero />
-             </Grid>
-             <Grid item container>
-                <Grid item xs={0} sm={1}/>
-                <Grid item xs={12} sm={10}>
-                   <Features />
-                </Grid>
-                <Grid item xs={0} sm={1} />
-             </Grid>
-         </Grid>
-        </Router>
-         
-    )
+  return (
+    <Router>
+      <Grid container direction='column'>
+        <Grid item container>
+          {" "}
+          <Header />
+        </Grid>
+        <Grid item container xs={12}>
+          <Hero />
+        </Grid>
+        <Grid item container>
+          <Grid item xs={0} sm={1} />
+          <Grid item xs={12} sm={10}>
+            <Features />
+          </Grid>
+          <Grid item xs={0} sm={1} />
+        </Grid>
+      </Grid>
+    </Router>
+  );
 }
 
-export default TodoBuilder
+export default TodoBuilder;
